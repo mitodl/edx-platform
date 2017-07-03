0fa063477db2a62afa599711b383f32385a5b54a
Gavin Sidebottom <gsidebo@mit.edu>
2017-07-03 15:40:22 -0400

    Enabled git auto-export upon course publish

--- cms/djangoapps/contentstore/signals/handlers.py
+++ cms/djangoapps/contentstore/signals/handlers.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from django.dispatch import receiver
 from pytz import UTC
-
+from django.conf import settings
 from contentstore.courseware_index import CoursewareSearchIndexer, LibrarySearchIndexer
 from contentstore.proctoring import register_special_exams
 from lms.djangoapps.grades.tasks import compute_all_grades_for_course
@@ -45,8 +45,13 @@ def listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=
     if CoursewareSearchIndexer.indexing_is_enabled():
         # import here, because signal is registered at startup, but items in tasks are not yet able to be loaded
         from contentstore.tasks import update_search_index
-
         update_search_index.delay(unicode(course_key), datetime.now(UTC).isoformat())
+
+    # If the Git auto-export is enabled, push the course changes to Git
+    if settings.FEATURES.get('ENABLE_EXPORT_GIT') and settings.FEATURES.get('ENABLE_GIT_AUTO_EXPORT'):
+        from .tasks import async_export_to_git
+        course_module = modulestore().get_course(course_key)
+        async_export_to_git.delay(course_module)
 
 
 @receiver(SignalHandler.library_updated)
