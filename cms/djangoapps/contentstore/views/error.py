663dfb0f9d266939d9215c3e30ea86a9566459ca
Gavin Sidebottom <gsidebo@mit.edu>
2017-12-29 16:30:04 -0500

    Fixed unthemed error pages

--- cms/djangoapps/contentstore/views/error.py
+++ cms/djangoapps/contentstore/views/error.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-docstring,unused-argument
 
 import functools
+import crum
 
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError
 
@@ -28,6 +29,21 @@ def jsonable_error(status=500, message="The Studio servers encountered an error"
     return outer
 
 
+def fix_crum_request(func):
+    """
+    A decorator that ensures that the 'crum' package (a middleware that stores and fetches the current request in
+    thread-local storate) can correctly fetch the current request. Under certain conditions, the current request cannot
+    be fetched by crum (e.g.: when HTTP errors are raised in our views via 'raise Http404', et. al.). This decorator
+    manually sets the current request for crum if it cannot be fetched.
+    """
+    @functools.wraps(func)
+    def wrapper(request, *args, **kwargs):
+        if not crum.get_current_request():
+            crum.set_current_request(request=request)
+        return func(request, *args, **kwargs)
+    return wrapper
+
+
 @jsonable_error(404, "Resource not found")
 def not_found(request):
     return render_to_response('error.html', {'error': '404'})
@@ -38,11 +54,13 @@ def server_error(request):
     return render_to_response('error.html', {'error': '500'})
 
 
+@fix_crum_request
 @jsonable_error(404, "Resource not found")
 def render_404(request):
     return HttpResponseNotFound(render_to_string('404.html', {}, request=request))
 
 
+@fix_crum_request
 @jsonable_error(500, "The Studio servers encountered an error")
 def render_500(request):
     return HttpResponseServerError(render_to_string('500.html', {}, request=request))
