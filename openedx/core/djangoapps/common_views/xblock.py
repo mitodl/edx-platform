ce587c31e5db59e4596269e8eb6e73d2e875d302
Gavin Sidebottom <gsidebo@mit.edu>
2018-02-21 13:38:41 -0500

    Refactored XBlockAside rendering and added support for student view

--- openedx/core/djangoapps/common_views/xblock.py
+++ openedx/core/djangoapps/common_views/xblock.py
@@ -9,6 +9,12 @@ import mimetypes
 from django.conf import settings
 from django.http import Http404, HttpResponse
 from xblock.core import XBlock
+from xblock.django.request import django_to_webob_request, webob_to_django_response
+from xblock.exceptions import NoSuchHandlerError
+
+from openedx.core.lib.xblock_utils import get_aside_from_xblock
+from xmodule.modulestore.django import modulestore
+from xmodule.modulestore.exceptions import ItemNotFoundError
 
 log = logging.getLogger(__name__)
 
@@ -31,3 +37,32 @@ def xblock_resource(request, block_type, uri):  # pylint: disable=unused-argumen
 
     mimetype, _ = mimetypes.guess_type(uri)
     return HttpResponse(content, content_type=mimetype)
+
+
+def invoke_xblock_aside_handler(request, usage_key, handler, suffix=''):
+    """
+    Dispatches a request to a handler specified in an XBlockAside, then returns the response.
+    """
+    req = django_to_webob_request(request)
+    try:
+        descriptor = modulestore().get_item(usage_key.usage_key)
+        aside_instance = get_aside_from_xblock(descriptor, usage_key.aside_type)
+        asides = [aside_instance] if aside_instance else []
+        resp = aside_instance.handle(handler, req, suffix)
+    except ItemNotFoundError:
+        log.warning(u'Unable to load xblock wrapped by aside [usage_key: %s]', usage_key.usage_key)
+        raise Http404
+    except NoSuchHandlerError:
+        log.info(
+            "XBlockAside %s attempted to access missing handler %r",
+            descriptor,
+            handler,
+            exc_info=True
+        )
+        raise Http404
+
+    # unintentional update to handle any side effects of handle call
+    # could potentially be updating actual course data or simply caching its values
+    modulestore().update_item(descriptor, request.user.id, asides=asides)
+
+    return webob_to_django_response(resp)
