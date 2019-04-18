5d9a341f6efd2a66f63bbf5ecdf8285e635ac7b1
Tobias Macey <tmacey@mit.edu>
2019-04-18 14:08:31 -0400

    Rapid response xBlock code changes
    This is aggregating a collection of changes for adding a tab to the instructor dashboard for reporting on submissions to rapid response problems.
    
    (cherry picked from commit 817b74e3cb9ba15f1071f5e940f0fa04f7b3ee86)
    
    Replacing deprecated permissions check for rapid response instructor view
    
    In Juniper the `require_level` decorator is removed in favor of the `require_course_permissions` decorator with more granular permissions definitions. Updated to use the new syntax in the rapid response instructor dashboard and remote gradebook modules.
    
    Removing explicit unicode handling in rapid response report
    
    The rapid response code was explicitly encoding report data unicode which is no longer necessary in Python 3
    
    Fix filename for rapid response CSV exports
    
    The file name for the Rapid Response CSV exports is being rendered as `b'rapid_response_submissions.csv'` due to extraneous encoding. Removed the call to encoding as we are now running in Python 3 so the name is UTF-8 by default.
    
    Use VIEW_DASHBOARD permission for rapid_response view (#187)
    
    removing unicodes from remote gradebook 659af001
    
    Fixing section iteration in remote gradebook 9b03ccf5
    
    Fixed 2to3 issues with remote gradebook and json response from gradebook 9819a244a
    
    Fixing settings type for remote gradebook plugin app
    
    Converting remote gradebooks to be python 3 compatible 7b9f46da
    
    changing remote_gradebook and canvas_integration modules importing w.r.t koa (https://discuss.openedx.org/t/koa-will-change-how-edx-platform-code-is-imported/3610)

--- lms/djangoapps/instructor_task/api.py
+++ lms/djangoapps/instructor_task/api.py
@@ -44,8 +44,8 @@ from lms.djangoapps.instructor_task.tasks import (
     reset_problem_attempts,
     send_bulk_course_email,
 )
-from remote_gradebook.constants import RGB_TASK_TYPES
-from canvas_integration.constants import CANVAS_TASK_TYPES
+from lms.djangoapps.remote_gradebook.constants import RGB_TASK_TYPES
+from lms.djangoapps.canvas_integration.constants import CANVAS_TASK_TYPES
 from util import milestones_helpers
 from xmodule.modulestore.django import modulestore
 
@@ -346,7 +346,7 @@ def submit_bulk_course_email(request, course_key, email_id):
     targets = Counter([target.target_type for target in email_obj.targets.all()])
     targets = [
         target if count <= 1 else
-        u"{} {}".format(count, target)
+        "{} {}".format(count, target)
         for target, count in six.iteritems(targets)
     ]
 
