579bad65c2b7c2f54fefd5534f291524b9dcbf53
Tobias Macey <tmacey@mit.edu>
2020-12-16 15:19:13 -0500

    Fixing encoding of header row for posting to remote gradebook
    Posting to the remote gradebook via lmod proxy was failing due to a key error when parsing the CSV content. This was due to erroneously encoding a string object. This commit adds a check for the type of the header value to prevent double encoding it.

--- lms/djangoapps/remote_gradebook/task_helpers.py
+++ lms/djangoapps/remote_gradebook/task_helpers.py
@@ -22,7 +22,8 @@ log = logging.getLogger(__name__)
 def create_datatable_csv(csv_file, datatable):
     """Creates a CSV file from the contents of a datatable."""
     writer = csv.writer(csv_file, dialect='excel', quotechar='"', quoting=csv.QUOTE_ALL)
-    encoded_row = [unicode(s).encode('utf-8') for s in datatable['header']]
+    encoded_row = [s if isinstance(s, str) else str(s).encode('utf-8')
+                   for s in datatable['header']]
     writer.writerow(encoded_row)
     for datarow in datatable['data']:
         # 's' here may be an integer, float (eg score) or string (eg student name)
