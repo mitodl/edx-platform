335f37c4f4a9fc7787c233ed96cbb68582695528
Hamza Farooq <42243411+HamzaIbnFarooq@users.noreply.github.com>
2021-02-25 17:27:32 +0500

    add LOGOUT_REDIRECT_URL (#241)

--- lms/envs/production.py
+++ lms/envs/production.py
@@ -1060,10 +1060,14 @@ COURSE_OLX_VALIDATION_IGNORE_LIST = ENV_TOKENS.get(
 ################# show account activate cta after register ########################
 SHOW_ACCOUNT_ACTIVATION_CTA = ENV_TOKENS.get('SHOW_ACCOUNT_ACTIVATION_CTA', SHOW_ACCOUNT_ACTIVATION_CTA)
 
-############## xPRO Base URL ############################
+############## xPRO Specific Variables ############################
 # This should be defined in ansible_vars for the xPRO app
 XPRO_BASE_URL = ENV_TOKENS.get('XPRO_BASE_URL')
 LOGIN_REDIRECT_WHITELIST = ENV_TOKENS.get(
     'LOGIN_REDIRECT_WHITELIST',
     LOGIN_REDIRECT_WHITELIST
+)
+LOGOUT_REDIRECT_URL = ENV_TOKENS.get(
+    'LOGOUT_REDIRECT_URL',
+    ''
 )
