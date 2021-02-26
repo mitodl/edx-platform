b916b7742c8cb3285d99dcfd7a0c62d32dc5873d
HamzaIbnFarooq <hamza.farooq@arbisoft.com>
2021-02-26 16:12:52 +0500

    making LOGIN_REDIRECT_WHITELIST configurable through env files

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -259,7 +259,7 @@ CORS_ALLOW_HEADERS = corsheaders_default_headers + (
     'use-jwt-cookie',
 )
 
-LOGIN_REDIRECT_WHITELIST = [
+LOGIN_REDIRECT_WHITELIST.extend([
     CMS_BASE,
     # Allow redirection to all micro-frontends.
     # Please add your MFE if is not already listed here.
@@ -275,7 +275,7 @@ LOGIN_REDIRECT_WHITELIST = [
     'localhost:18400',  # frontend-app-publisher
     ENTERPRISE_LEARNER_PORTAL_NETLOC,  # frontend-app-learner-portal-enterprise
     ENTERPRISE_ADMIN_PORTAL_NETLOC,  # frontend-app-admin-portal
-]
+])
 
 ###################### JWTs ######################
 JWT_AUTH.update({
